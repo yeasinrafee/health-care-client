db322f10da58132d593e3852ac3d413547e028b8
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-02-04 01:03:22 +0600

    Navbar and footer complete

--- src/app/(withCommonLayout)/layout.tsx
+++ src/app/(withCommonLayout)/layout.tsx
@@ -1,12 +1,14 @@
+import Footer from '@/components/shared/Footer/Footer';
 import Navbar from '@/components/shared/Navbar/Navbar';
 import { ReactNode } from 'react';
 
 const CommonLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div>
+    <>
       <Navbar />
-      {children}
-    </div>
+      <div className='min-h-screen'>{children}</div>
+      <Footer />
+    </>
   );
 };
 
