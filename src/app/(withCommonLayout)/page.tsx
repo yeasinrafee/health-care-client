5989e42b45f42d2e6b2b7f0635e9280fd2b20b4c
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-02-05 02:18:42 +0600

    Why choose us

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from '@/components/ui/HomePage/HeroSection/HeroSection';
 import Specialist from '@/components/ui/HomePage/Specialist/Specialist';
 import TopRatedDoctors from '@/components/ui/HomePage/TopRatedDoctors/TopRatedDoctors';
+import WhyUs from '@/components/ui/HomePage/WhyUs/WhyUs';
 
 const Homepage = () => {
   return (
@@ -8,6 +9,7 @@ const Homepage = () => {
       <HeroSection />
       <Specialist />
       <TopRatedDoctors />
+      <WhyUs />
     </>
   );
 };
