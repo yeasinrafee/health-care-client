513e933e5046867fb3b3501d226f383df3a0aeed
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-02-04 12:03:40 +0600

    Specialist section

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from '@/components/ui/HomePage/HeroSection/HeroSection';
+import Specialist from '@/components/ui/HomePage/Specialist/Specialist';
 
 const Homepage = () => {
   return (
     <>
       <HeroSection />
+      <Specialist />
     </>
   );
 };
