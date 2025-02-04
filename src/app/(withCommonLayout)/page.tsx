eb06ac494cb6da0b6cd3bd9792446f22b217fd30
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-02-05 00:29:37 +0600

    Create Top rated doctors section

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from '@/components/ui/HomePage/HeroSection/HeroSection';
 import Specialist from '@/components/ui/HomePage/Specialist/Specialist';
+import TopRatedDoctors from '@/components/ui/HomePage/TopRatedDoctors/TopRatedDoctors';
 
 const Homepage = () => {
   return (
     <>
       <HeroSection />
       <Specialist />
+      <TopRatedDoctors />
     </>
   );
 };
