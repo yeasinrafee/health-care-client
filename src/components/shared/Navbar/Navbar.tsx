9001a8f8e099c5d548924ca5e167e312d5e9a0e1
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-02-07 00:13:17 +0600

    Retrive and Decode User from Local storage

--- src/components/shared/Navbar/Navbar.tsx
+++ src/components/shared/Navbar/Navbar.tsx
@@ -1,7 +1,11 @@
+'use client';
+import { getUserInfo } from '@/services/auth.services';
 import { Box, Button, Container, Stack, Typography } from '@mui/material';
 import Link from 'next/link';
 
 const Navbar = () => {
+  const userInfo = getUserInfo();
+  console.log(userInfo);
   return (
     <Container>
       <Stack
@@ -34,9 +38,15 @@ const Navbar = () => {
             NGOs
           </Typography>
         </Stack>
-        <Button component={Link} href='/login'>
-          Login
-        </Button>
+        {userInfo.userId ? (
+          <Button color='error' component={Link} href='/login'>
+            Logout
+          </Button>
+        ) : (
+          <Button component={Link} href='/login'>
+            Login
+          </Button>
+        )}
       </Stack>
     </Container>
   );
--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -1,6 +1,33 @@
 import { authKey } from '@/constants/authkey';
-import { setToLocalStorage } from '@/utils/local-storage';
+import { decodeToken } from '@/utils/jwt';
+import {
+  getFormLocalStorage,
+  removeFormLocalStorage,
+  setToLocalStorage,
+} from '@/utils/local-storage';
 
 export const storeUserInfo = ({ accessToken }: { accessToken: string }) => {
   return setToLocalStorage(authKey, accessToken);
+};
+
+export const getUserInfo = () => {
+  const authToken = getFormLocalStorage(authKey);
+  if (authToken) {
+    const decodedData: any = decodeToken(authToken);
+    return {
+      ...decodedData,
+      role: decodedData?.role.toLowerCase(),
+    };
+  }
+};
+
+export const isLoggedIn = () => {
+  const authToken = getFormLocalStorage(authKey);
+  if (authToken) {
+    return !!authToken;
+  }
+};
+
+export const removeUser = () => {
+  return removeFormLocalStorage(authKey);
 };
