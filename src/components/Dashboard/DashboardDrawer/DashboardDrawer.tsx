af67f00e9a065214d9394abb02297a6ff9e44228
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-02-10 10:07:05 +0600

    Sidebar with menuItem

--- src/components/Dashboard/DashboardDrawer/DashboardDrawer.tsx
+++ src/components/Dashboard/DashboardDrawer/DashboardDrawer.tsx
@@ -3,19 +3,12 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import MailIcon from '@mui/icons-material/Mail';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import { IconButton } from '@mui/material';
+import SideBar from '../SideBar/SideBar';
 
 const drawerWidth = 240;
 
@@ -41,40 +34,6 @@ export default function DashboardDrawer({
       setMobileOpen(!mobileOpen);
     }
   };
-
-  const drawer = (
-    <div>
-      <Toolbar />
-      <Divider />
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
-
-  // Remove this const when copying and pasting into your project.
 
   return (
     <Box sx={{ display: 'flex' }}>
@@ -123,7 +82,7 @@ export default function DashboardDrawer({
             },
           }}
         >
-          {drawer}
+          <SideBar />
         </Drawer>
         <Drawer
           variant='permanent'
@@ -136,7 +95,7 @@ export default function DashboardDrawer({
           }}
           open
         >
-          {drawer}
+          <SideBar />
         </Drawer>
       </Box>
       <Box
---
+++ src/components/Dashboard/SideBar/SideBar.tsx
@@ -0,0 +1,73 @@
+import {
+  Box,
+  Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Stack,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import React from 'react';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import assets from '@/assets';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const SideBar = () => {
+  const drawer = (
+    <div>
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+  return (
+    <Box>
+      <Stack
+        sx={{
+          py: 1,
+          mt: 1,
+        }}
+        direction='row'
+        justifyContent='center'
+        alignItems='center'
+        component={Link}
+        href='/'
+      >
+        <Image src={assets.svgs.logo} width={40} height={40} alt='logo' />
+        <Typography variant='h6' component='h1' sx={{ cursor: 'pointer' }}>
+          Emperal Health Care
+        </Typography>
+      </Stack>
+      {drawer}
+    </Box>
+  );
+};
+
+export default SideBar;
