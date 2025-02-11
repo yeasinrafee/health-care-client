1452f8dfa370ba670129b8cb0d947f571897dfd1
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-02-11 22:49:29 +0600

    Generating menu items based on user roles

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,4 +1,6 @@
 import { USER_ROLE } from '@/constants/role';
+import { SvgIconTypeMap } from '@mui/material';
+import { OverridableComponent } from '@mui/material/OverridableComponent';
 
 export type TMeta = {
   page: number;
@@ -7,3 +9,11 @@ export type TMeta = {
 };
 
 export type TUserRole = keyof typeof USER_ROLE;
+
+export type TDrawerItem = {
+  title: string;
+  path: string;
+  parentPath?: string;
+  icon?: OverridableComponent<SvgIconTypeMap<{}, 'svg'>> & { muiName: string };
+  child?: TDrawerItem[];
+};
