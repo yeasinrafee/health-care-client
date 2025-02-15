e111377dde8da1d0cedd6a9acff927a56282dc6c
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-02-15 15:30:54 +0600

    Date formatter

--- src/app/(withDashboardLayout)/dashboard/admin/schedules/components/ScheduleModal.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/schedules/components/ScheduleModal.tsx
@@ -1,6 +1,8 @@
 import HCDatePicker from '@/components/Forms/HCDatePicker';
 import HCForm from '@/components/Forms/HCForm';
+import HCTimePicker from '@/components/Forms/HCTimePicker';
 import HCModal from '@/components/shared/HCModal/HCModal';
+import { dateFormatter } from '@/utils/DateFormatter';
 import { Button, Grid } from '@mui/material';
 import React from 'react';
 import { FieldValues } from 'react-hook-form';
@@ -12,7 +14,11 @@ type TProps = {
 
 const ScheduleModal = ({ open, setOpen }: TProps) => {
   const handleFormSubmit = async (values: FieldValues) => {
-    console.log(values);
+    // console.log(dateFormatter(values.startDate));
+
+    values.startDate = dateFormatter(values.startDate);
+    values.endDate = dateFormatter(values.endDate);
+
     try {
     } catch (err: any) {
       console.log(err);
@@ -21,12 +27,23 @@ const ScheduleModal = ({ open, setOpen }: TProps) => {
   return (
     <HCModal open={open} setOpen={setOpen} title='Create Schedule'>
       <HCForm onSubmit={handleFormSubmit}>
-        <Grid container spacing={2}>
+        <Grid container spacing={2} sx={{ width: '400px' }}>
           <Grid item md={12}>
-            <HCDatePicker name='startDate' />
+            <HCDatePicker name='startDate' label='Start Date' />
+          </Grid>
+          <Grid item md={12}>
+            <HCDatePicker name='endDate' label='End Date' />
+          </Grid>
+          <Grid item md={6}>
+            <HCTimePicker name='startTime' label='Start Time' />
+          </Grid>
+          <Grid item md={6}>
+            <HCTimePicker name='endTime' label='End Time' />
           </Grid>
         </Grid>
-        <Button type='submit'>Create</Button>
+        <Button sx={{ mt: 1 }} type='submit'>
+          Create
+        </Button>
       </HCForm>
     </HCModal>
   );
--- src/components/Forms/HCTimePicker.tsx
+++ src/components/Forms/HCTimePicker.tsx
@@ -1,28 +1,30 @@
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 import dayjs from 'dayjs';
 import { SxProps } from '@mui/material';
 import { Controller, useFormContext } from 'react-hook-form';
 
-type TDatePicker = {
+type TTimePicker = {
   name: string;
   size?: 'small' | 'medium';
+  placeholder?: string;
   label?: string;
   required?: boolean;
   fullWidth?: boolean;
   sx?: SxProps;
 };
 
-const HCDatePicker = ({
+const HCTimePicker = ({
   name,
   size = 'small',
   label,
   required,
   fullWidth = true,
   sx,
-}: TDatePicker) => {
-  const { control } = useFormContext();
+}: TTimePicker) => {
+  const { control, formState } = useFormContext();
+  const isError = formState.errors[name] !== undefined;
   return (
     <Controller
       name={name}
@@ -31,12 +33,11 @@ const HCDatePicker = ({
       render={({ field: { onChange, value, ...field } }) => {
         return (
           <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DesktopDatePicker
+            <TimePicker
               label={label}
               timezone='system'
-              disablePast
               {...field}
-              onChange={(date) => onChange(date)}
+              onChange={(time) => onChange(time)}
               value={value || Date.now()}
               slotProps={{
                 textField: {
@@ -57,4 +58,4 @@ const HCDatePicker = ({
   );
 };
 
-export default HCDatePicker;
+export default HCTimePicker;
